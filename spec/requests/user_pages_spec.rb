fae90e598cc1b5e5fbec2743fa429fe5b7979e0d
Liu Jianming <ivanliu0626@hotmail.com>
2014-08-27 02:17:50 -0700

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,8 +9,6 @@ describe "User Pages" do
     let(:user) { FactoryGirl.create(:user) }
 
     before do
-    let(:user) { FactoryGirl.create(:user) }
-    before(:each) do
       sign_in user
       visit users_path
     end
@@ -54,11 +52,18 @@ describe "User Pages" do
   end
 
   describe "profile page" do
-    let(:user) {FactoryGirl.create(:user)}
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
     before { visit user_path(user)}
 
     it { should have_content(user.name)}
     it { should have_title(user.name)} 
+    describe "micropost" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
@@ -66,7 +71,6 @@ describe "User Pages" do
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
-
   end
 
   describe "signup" do
